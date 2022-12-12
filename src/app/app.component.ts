030dfe0f87aef9ebd5faa3d5224b13db53f98bab
iamrk-js <88290009+iamrk-js@users.noreply.github.com>
2022-12-12 16:51:40 +0530

    confirmpassword disabel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { genders } from './shared/constants/genders';
 import { CountriesService } from './shared/services/countries.service';
 import { EmplIdValidtors } from './shared/validations/emplIdValidator';
+import { passwordMatch } from './shared/validations/passwordMatch';
 import { SpaceValidator } from './shared/validations/spaceValidator';
 import { CustomRegex } from './validation-regax';
 
@@ -16,16 +17,29 @@ export class AppComponent implements OnInit {
   title = 'reactiveForm';
   signUpForm !: FormGroup;
   gendersArray: string[] = [genders.FEMALE, genders.MALE, genders.OTHERS]
-  countryNames: string[] = []
+  countryNames: string[] = [];
+  passwordValidator = Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$');
   constructor(private countryService: CountriesService) {
 
   }
   ngOnInit(): void {
     this.countryNames = this.countryService.getAllConuntryNames();
     this.onSkillAdd();
+    this.createSignUp();
+    this.f['password'].valueChanges
+      .subscribe(res => {
+        console.log(this.f['password']?.valid);
+        if(this.f['password']?.valid){
+          this.f['confirmPassword']?.enable()
+        }
+      })
+
+  }
+
+  createSignUp() {
     this.signUpForm = new FormGroup({
       username: new FormControl('', [Validators.required, SpaceValidator.noSpaceValidation]),
-      employeeId : new FormControl("", [Validators.required, Validators.minLength(4), Validators.maxLength(4), EmplIdValidtors.idValidate]),
+      employeeId: new FormControl("", [Validators.required, Validators.minLength(4), Validators.maxLength(4), EmplIdValidtors.idValidate]),
       email: new FormControl(null, [Validators.required, Validators.pattern(CustomRegex.email)]),
       gender: new FormControl(null),
       address: new FormGroup({
@@ -35,19 +49,18 @@ export class AppComponent implements OnInit {
         zipcode: new FormControl(null, Validators.required)
       }),
       skills: new FormArray([new FormControl(null, Validators.required)]),
-      password : new FormControl(null, [Validators.required, Validators.minLength(8)]),
-      confirmPassword : new FormControl(null, [Validators.required, Validators.minLength(8)]),
-    })
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl({ value: null, disabled: true }, [Validators.required, Validators.minLength(8)]),
+    }, [passwordMatch('password', 'confirmPassword')])
   }
-
   onSignUp() {
     console.log(this.signUpForm);
   }
-  onSkillAdd(){
+  onSkillAdd() {
     let control = new FormControl(null, [Validators.required])
     this.skillsArray?.push(control);
   }
-  onSkillRemove(index:number){
+  onSkillRemove(index: number) {
     this.skillsArray.removeAt(index)
   }
   get f() {
@@ -72,7 +85,7 @@ export class AppComponent implements OnInit {
   get zipcodeControl() {
     return this.signUpForm?.get('address')?.get('zipcode') as FormControl;
   }
-  get skillsArray(){
+  get skillsArray() {
     return this.signUpForm?.get('skills') as FormArray
   }
 }
--- src/app/shared/validations/emplIdValidator.ts
+++ src/app/shared/validations/emplIdValidator.ts
@@ -2,11 +2,26 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 
 
 export class EmplIdValidtors {
-    static idValidate(control : AbstractControl) : ValidationErrors | null{
+    static idValidate(control: AbstractControl): ValidationErrors | null {
         let regexp = /^[a-z]\d{3}$/i
         let val = control.value as string;
         let isValid = regexp.test(val)
 
-        return isValid ? null : {invalidid : true}
+        return isValid ? null : { invalidid: true }
+    }
+
+     passwordMatchValidator(control: AbstractControl) {
+        const password = control?.get('password')?.value;
+        const confirmPassword = control?.get('confirmPassword')?.value;
+
+        if(password != confirmPassword){
+            control?.get('confirmPassword')?.setErrors({NoPasswordMatch : true})
+        }else{
+            control?.get('confirmPassword')?.setErrors(null)
+        }
+
     }
 }
+
+
+
